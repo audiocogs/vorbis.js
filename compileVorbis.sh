f78f14163e2706071964a549f09f9504169b49aa
William Toohey <will@mon.im>
2016-02-24 22:46:25 +1000

    Fix Linux build

--- compileVorbis.sh
+++ compileVorbis.sh
@@ -8,11 +8,11 @@ if [ ! -f configure ]; then
   
   # -O20 and -04 cause problems
   # see https://github.com/kripken/emscripten/issues/264
-  sed -i '' 's/-O20/-O2/g' configure
-  sed -i '' 's/-O4/-O2/g' configure
+  perl -pi -e s,-O20,-O2,g configure
+  perl -pi -e s,-O4,-O2,g configure
   
   # finally, run configuration script
-  emconfigure ./configure --prefix="`pwd`" --disable-static
+  emconfigure ./configure --prefix="`pwd`/build" --disable-static
 fi
 
 # compile libogg
@@ -27,20 +27,20 @@ cd libvorbis
 if [ ! -f configure ]; then
   # generate configuration script
   # disable running configure automatically
-  sed -i '' 's/$srcdir\/configure/#/' autogen.sh
+  perl -pi -e 's,\$srcdir\,configure,#,' autogen.sh
   ./autogen.sh
   
   # -O20 and -04 cause problems
   # see https://github.com/kripken/emscripten/issues/264
-  sed -i '' 's/-O20/-O2/g' configure
-  sed -i '' 's/-O4/-O2/g' configure
+  perl -pi -e s,-O20,-O2,g configure
+  perl -pi -e s,-O4,-O2,g configure
   
   # disable oggpack_writealign test
-  sed -i '' 's/$ac_cv_func_oggpack_writealign/yes/' configure
+  perl -pi -e 's,\$ac_cv_func_oggpack_writealign,yes,' configure
   
   # finally, run configuration script
   mkdir -p build
-  emconfigure ./configure --prefix="$dir/libvorbis/build" --disable-oggtest --disable-static --with-ogg=$dir/libogg --with-ogg-libraries=$dir/libogg/lib
+  emconfigure ./configure --prefix="$dir/libvorbis/build" --disable-oggtest --disable-static --with-ogg=$dir/libogg --with-ogg-libraries=$dir/libogg/build/lib
 fi
 
 # compile libvorbis
@@ -50,5 +50,5 @@ emmake make install
 # compile wrapper
 cd ..
 mkdir -p build
-emcc -O3 -s RESERVED_FUNCTION_POINTERS=50 -s EXPORTED_FUNCTIONS="['_VorbisInit', '_VorbisHeaderDecode', '_VorbisGetChannels', '_VorbisGetSampleRate', '_VorbisGetNumComments', '_VorbisGetComment', '_VorbisDecode', '_VorbisDestroy']" -I libogg/include -Llibogg/lib -logg -I libvorbis/include -Llibvorbis/build/lib -lvorbis src/vorbis.c -o build/libvorbis.js
+emcc -O3 --memory-init-file 0 -s RESERVED_FUNCTION_POINTERS=50 -s EXPORTED_FUNCTIONS="['_VorbisInit', '_VorbisHeaderDecode', '_VorbisGetChannels', '_VorbisGetSampleRate', '_VorbisGetNumComments', '_VorbisGetComment', '_VorbisDecode', '_VorbisDestroy']" -I libogg/include -Llibogg/build/lib -logg -I libvorbis/include -Llibvorbis/build/lib -lvorbis src/vorbis.c -o build/libvorbis.js
 echo "module.exports = Module" >> build/libvorbis.js
